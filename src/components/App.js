c2cc85b315c0f8bdba323d99d9871b70838fb686
Daniel Luiz <danielluizthiessen@gmail.com>
2021-05-01 11:40:46 -0300

    Criei os arquivos da componentização e comecei a componentizar

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+
+import Navbar from "./Navbar";
+import Content from "./Content";
+import Mobile from "./Mobile";
+
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+            <Content />
+            <Mobile />
+        </div>
+        );  
+}
